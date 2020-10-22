51bdb77ff4621c69308d8f1315492cb7ef28ef5f
Larissa <lari.cmarcondes@outlook.com>
2020-10-21 21:57:07 -0300

    atualização filmes api, documentos front e adição do projeto conexao vagas em react.js(21-10)

---
+++ Sprint3 - Front/React.Js/ConexaoVagas/ConexaoVagas-Front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/tailwind.css';
+import Routers from './routers';
+
+function App() {
+  return (
+    <div className="App">
+      <Routers></Routers>
+    </div>
+  );
+}
+
+export default App;
---
+++ Sprint3 - Front/React.Js/ConexaoVagas/ConexaoVagas-Front/src/routers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+function Routers(){
+    return(
+        <BrowserRouter>
+            {/* <Route path="/" exact component={Home} />
+            <Route path="/login"  component={Login} />
+            <Route path="/cadastro"  component={Cadastro} /> */}
+        </BrowserRouter>
+    )  
+}
+
+export default Routers;
